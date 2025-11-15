9e9531d6f362b6ca2d3f97b23b2550b641a6f118
Aditi Jha <aditijha934@gmail.com>
2025-11-15 11:57:00 +0530

    Length of a string, gets, puts, fgets

--- C language.c
+++ C language.c
@@ -1,3 +1,20 @@
-//
-// Created by Aditi Jha on 15/11/25.
-//
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int count = 0;
+    char s1[12];
+    printf("Enter string to generate: ");
+    fgets(s1,sizeof(s1), stdin);
+    count = strlen(s1);
+    printf("The length of the string is: %d\n", count);
+    return 0;
+
+
+    int i,
+    i = 0;
+    while (s1[i] != '\0') {
+        i++;
+        count++;
+    }
+    printf("%d", count);
+}
